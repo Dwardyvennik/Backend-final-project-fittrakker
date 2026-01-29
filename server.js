6d5a4edbe767f52aaf6958eb0c3826d7b207fd9f
Ali Sh <ali_k@dwardy>
2026-01-29 17:30:08 +0500

    Change html files

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
-
 app.use(express.static("public"));
 
 app.use("/api/workouts", workoutsRoutes);

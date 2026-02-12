dab95279380b2b746ec2908e7d5b6a4cbb6ebe7e
Ali Sh <ali_k@dwardy>
2026-02-12 17:23:13 +0500

    debug

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (!process.env.SESSION_SECRET) {
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.set("trust proxy", 1); 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
